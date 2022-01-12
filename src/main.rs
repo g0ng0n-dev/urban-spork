37a80c636048bf81561958d4f9c0659ab82f9972
Gon Gis <g0ng0n.dev@gmail.com>
2022-01-12 11:19:37 +0100

    initial commit with an echo tcp server

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+use std::net::TcpStream;
+use tokio::io::AsyncWriteExt;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let listener= TcpListener::bind("localhost:8080").await.unwrap();
+    let (mut socket,addr ) = listener.accept().await.unwrap();
+
+    loop {
+        let mut buffer = [0u8, 1024];
+
+        let bytes_read = socket.read(&mut buffer).await.unwrap();
+
+        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+    }
+
+}
