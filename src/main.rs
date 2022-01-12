0688ff6647a03dc96e07573ace92d55a6045229f
Gon Gis <g0ng0n.dev@gmail.com>
2022-01-12 11:30:57 +0100

    handling multiple lines

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::net::TcpStream;
-use tokio::io::AsyncWriteExt;
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
 
 #[tokio::main]
@@ -7,12 +7,22 @@ async fn main() {
     let listener= TcpListener::bind("localhost:8080").await.unwrap();
     let (mut socket,addr ) = listener.accept().await.unwrap();
 
+    let (reader, mut write) = socket.split();
+    // The Bufreader of tokyo wraps any kind of reader and it maintains its own buffer
+    // and allows you to do some higher level read operations, for example read an entire line of text
+    let mut reader = BufReader::new(&reader);
+
+    let mut line = String::new();
     loop {
-        let mut buffer = [0u8, 1024];
 
-        let bytes_read = socket.read(&mut buffer).await.unwrap();
+        let bytes_read = reader.read_line(&mut line).await.unwrap();
+        if bytes_read == 0{
+            break;
+        }
+        write.write_all(line.as_bytes()).await.unwrap();
 
-        socket.write_all(&buffer[..bytes_read]).await.unwrap();
+        //we need to clear out the input buffer
+        line.clear()
     }
 
 }
